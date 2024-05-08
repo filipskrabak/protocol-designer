54c4d1cc1cba42a3a9c69dcda622bbc012fa59c6
filipskrabak <skrabak.filip@gmail.com>
2024-05-08 15:13:26 +0200

    fix: deprecated code

--- backend/src/auth/jwthandler.py
+++ backend/src/auth/jwthandler.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from typing import Optional
 
 from fastapi import Depends, HTTPException, Request
@@ -8,7 +8,7 @@ from fastapi.security import OAuth2
 from fastapi.security.utils import get_authorization_scheme_param
 from jose import JWTError, jwt
 
-from src.schemas import TokenData
+from src.schemas import Status, TokenData
 from src.schemas import UserOut
 from src.models import User
 
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 43200 # 30 days
 
-
+# Code taken from FastAPI documentation
 class OAuth2PasswordBearerCookie(OAuth2):
     def __init__(
         self,
@@ -58,9 +58,9 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
 
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
 
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
