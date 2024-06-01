9f2746cb47edea30f047f6e411757bebc51f16c0
filipskrabak <skrabak.filip@gmail.com>
2024-06-01 15:18:28 +0200

    feat: footer icons

--- frontend/src/layouts/guest.vue
+++ frontend/src/layouts/guest.vue
@@ -6,9 +6,29 @@
 
     <v-footer class="bg-grey-lighten-4" style="max-height: 150px">
       <v-row justify="center" no-gutters>
-        <v-col class="text-center mt-4" cols="12">
-          {{ new Date().getFullYear() }} — <strong>Protocol Designer</strong>
-        </v-col>
+        <v-row>
+          <v-col class="text-center mt-4" cols="12">
+            {{ new Date().getFullYear() }} — <strong>Protocol Designer</strong>
+          </v-col>
+          <v-col class="text-center" cols="12">
+            <v-btn
+              icon="mdi-github"
+              class="mx-4"
+              variant="text"
+              href="https://github.com/filipskrabak/protocol-designer"
+              target="_blank"
+
+            ></v-btn>
+            <v-btn
+              class="mx-4"
+              href="https://www.fiit.stuba.sk"
+              target="_blank"
+              variant="text"
+            >
+              <v-img src="/images/STU-FIIT-zcv.png" width="60"></v-img>
+          </v-btn>
+          </v-col>
+        </v-row>
       </v-row>
     </v-footer>
 
