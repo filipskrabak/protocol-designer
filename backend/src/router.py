9d6f7bb44af434922b616d8f1776c311d32b9f35
filipskrabak <skrabak.filip@gmail.com>
2025-09-15 13:48:20 +0200

    chore: optimize downtime during deployments

--- backend/src/router.py
+++ backend/src/router.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from src.endpoints import users
 from src.endpoints import protocols
 from src.endpoints import protocol_encapsulations
+from src.endpoints import health
 
 router = APIRouter()
 
+router.include_router(health.router, tags=["health"])
 router.include_router(users.router, tags=["users"])
 router.include_router(protocols.router, tags=["protocols"])
 router.include_router(protocol_encapsulations.router, tags=["protocol encapsulations"])
